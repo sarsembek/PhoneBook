80562cf054c6bcf42f1e59fc50234bd2fe4c137d
Arman <ar_sarsembek@kbtu.kz>
2023-12-12 01:11:16 +0600

    zipkin added

--- src/main/java/kz/kbtu/phonebook/service/UserService.java
+++ src/main/java/kz/kbtu/phonebook/service/UserService.java
@@ -1,28 +1,35 @@
 package kz.kbtu.phonebook.service;
 import kz.kbtu.phonebook.model.User;
 import kz.kbtu.phonebook.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
 
     @Autowired
     private EventPublisher eventPublisher;
+
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    @NewSpan("get-users")
     public List<User> getAllUsers() {
+        log.info("Users retrieved");
         return (List<User>) userRepository.findAll();
     }
 
+    @NewSpan("get-user-by-id")
     public User getUserById(Long id) {
+        log.info(String.format("User with id: %d",id));
         return userRepository.findById(id).orElse(null);
     }
 
